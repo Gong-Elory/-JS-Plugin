826431e179fb74581435f3bd5138f2a6dd6735c9
龚志强 <elory0513@hotmail.com>
2016-12-03 09:58:00 +0800

    线性运动js原生 -v-0.20

--- animquery.js
+++ animquery.js
@@ -22,6 +22,7 @@
 
 function moveLinear(targetElements,obj,func) {
         var onlyOneTest = true;
+
         clearInterval(targetElements.timer);
         targetElements.timer = setInterval(function () {
             var allDoneFlag = true;
@@ -78,3 +79,134 @@ function getCssValue(element,attr) {
     styleValue = (attr == "opacity")?parseInt(parseFloat(styleValue)*100):parseInt(styleValue);
     return styleValue;
 }
+
+/*
+*   name: scrollLinear
+*   description: You can use this function to control the object to scroll from any direction.
+*   params:
+*       elementContainer:
+*                       a object contains 'ul' object
+*       targetElement:
+*                      a 'ul' object which contains list of "li" object
+*       direction:
+*                   scrolling direction default: [u2d] means：up-to-down
+*                   you can use this:
+*                                               [d2u] means:down-to-up
+*                                               [r2l] means:right-to-left
+*                                               [l3r] means:left-to-right
+*       speed: scrolling speed. default: 30
+*       obj:   a json object which contains attrbutes about intermittent scrolling.
+*               attributes:
+*                   [mode]: you can setting the  number of scrolling intervals. default: -1 :no intervals
+*                   [delay]: the time of scrolling intervals （The unit is milliseconds）
+*       return: no return
+*
+*       template:
+*           ······scrollLinear(divElem,ulElem)
+*           ······scrollLinear(divElem,ulElem,"u2d")
+*           ······scrollLinear(divElem,ulElem,"u2d",40)
+*           ······scrollLinear(divElem,ulElem,"u2d",{"mode":2})
+*           ······scrollLinear(divElem,ulElem,"u2d",{"mode":2,"delay":1000})
+ */
+
+function scrollLinear(elementContainer,targetElement,direction,speed,obj) {
+    var speedValue = 30,
+        inn,
+        stepTest = 0,
+        mode=-1,
+        delay = 1000;
+    elementContainer.timer = null;
+    if(speed) speedValue = speed;
+    if(obj){
+        mode = obj.mode?obj.mode:-1;
+        var delay = obj.delay?obj.delay:1000;
+        console.log(mode+":"+delay);
+    }
+    targetElement.innerHTML += targetElement.innerHTML;
+    var num = targetElement.getElementsByTagName("li").length;
+    switch(direction){
+        case "r2l":
+        case "l2r":
+                    var widthElem = targetElement.getElementsByTagName("li")[0].offsetWidth;
+                    var leftValue = parseInt(targetElement.getElementsByTagName("li")[0].style.marginLeft);
+                    var rightValue =parseInt(targetElement.getElementsByTagName("li")[0].style.marginRight);
+                    if(rightValue) widthElem+=rightValue;
+                    if(leftValue ) widthElem+=leftValue ;
+                    inn = widthElem*num;
+                    targetElement.style.width = inn+"px";
+                    break;
+        case "d2u":
+        case "u2d":
+        default:
+                    var heightElem = targetElement.getElementsByTagName("li")[0].offsetHeight
+                    var topValue = parseInt(targetElement.getElementsByTagName("li")[0].style.marginTop);
+                    var bottomValue= parseInt(targetElement.getElementsByTagName("li")[0].style.marginBottom);
+                    if(topValue) heightElem+=topValue;
+                    if(bottomValue) heightElem+=bottomValue ;
+                    inn = heightElem*num;
+                    targetElement.style.height = inn+"px";
+                    break;
+    }
+    if(mode == -1){
+        elementContainer.timer = setInterval(scrollStep,speedValue);
+    }else if(mode > 0){
+        elementContainer.timer = setTimeout(scrollByStep,delay);
+    }
+
+    elementContainer.onmouseover= function () {
+        // if(stepTest == 2) clearTimeout(elementContainer.timer);
+        // else clearInterval(elementContainer.timer);
+        if(stepTest == 1) clearTimeout(elementContainer.timer);
+        else clearInterval(elementContainer.timer);
+
+    }
+    elementContainer.onmouseleave = function () {
+
+        if(stepTest == 1) elementContainer.timer = setTimeout(scrollByStep,speedValue);
+        else elementContainer.timer = setInterval(scrollStep,speedValue);
+
+
+    }
+
+    function scrollStep() {
+        stepTest = 2;
+        switch(direction){
+            case "r2l" :
+                if((elementContainer.scrollLeft++) > (inn/2)) elementContainer.scrollLeft = 0;
+                else if((mode > 0) && (elementContainer.scrollLeft %( mode * widthElem)) == 0 ) {
+                    clearInterval(elementContainer.timer);
+                    setTimeout(scrollByStep, delay);
+                }
+                break;
+            case "l2r" :
+                if((elementContainer.scrollLeft--) <= 0) elementContainer.scrollLeft =(inn/2);
+                else if((mode > 0) && ((elementContainer.scrollLeft) %( mode * widthElem)) == 0){
+                    clearInterval(elementContainer.timer);
+                    setTimeout(scrollByStep,delay);
+                }
+                break;
+            case "d2u" :
+                if((elementContainer.scrollTop++) > (inn/2)) {elementContainer.scrollTop= 0;stepTest = 1;}
+                else if((mode > 0) && (elementContainer.scrollTop %( mode * heightElem)) == 0 ) {
+                    clearInterval(elementContainer.timer);
+                    elementContainer.timer = setTimeout(scrollByStep, delay);
+                }
+                break;
+            case "u2d" :
+            default:
+                if((elementContainer.scrollTop--) <= 0) elementContainer.scrollTop= (inn/2);
+                else if((mode > 0) && (elementContainer.scrollTop %( mode * heightElem)) == 0 ){
+                    clearInterval(elementContainer.timer);
+                    setTimeout(scrollByStep,delay);
+                }
+                break;
+        }
+    }
+
+    function scrollByStep()
+    {
+        stepTest = 1;
+        elementContainer.timer=setInterval(scrollStep,speedValue);
+    }
+}
+
